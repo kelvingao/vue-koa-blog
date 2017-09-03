915c2fd1f9b0ec16ea1984c750f96ae3b6ce76af
kelvingao <89156201@qq.com>
2017-09-03 20:52:01 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <app-header></app-header>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import Footer from './components/footer.vue'
+import Header from './components/header.vue'
+
+export default {
+  components: {
+    'app-header': Header,
+    'app-footer': Footer
+  },
+  data () {
+    return {
+    
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/footer.vue
@@ -0,0 +1,29 @@
+<template>
+    <footer>
+        <p> Copyright &copy; {{ copyright }} </p>
+    </footer>     
+
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+        copyright: '2017 Kelvin Gao'
+    }
+  }
+}
+</script>
+    
+ 
+<style scoped>
+footer {
+    background: #222;
+    padding: 6px;
+}
+p {
+    color: lightgreen;
+    text-align: center;
+}
+
+</style>
